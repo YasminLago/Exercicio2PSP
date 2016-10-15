493281d2fc59d50b057b869a097b74e713a39229
YasminLago <ylagorebollar@danielcastelao.org>
2016-10-15 18:32:19 +0200

    Update Thread2.java

--- src/exercicio2psp/Thread2.java
+++ src/exercicio2psp/Thread2.java
@@ -15,14 +15,13 @@ public class Thread2 extends Thread{
     public void run(){
         for(int i=0;i<5;i++){   
         try {
-            System.out.println("Numero de iteracion: "+i +" Nome: "+getName());            
-            Thread.sleep(1000);
+            System.out.println("Numero de iteracion: "+i +" Nome: "+getName());           
         } catch (InterruptedException ex) {
             Logger.getLogger(Thread2.class.getName()).log(Level.SEVERE, null, ex);
         }
         }
         try {
-            Thread.sleep(1000);
+            Thread.sleep(2000);
         } catch (InterruptedException ex) {
             Logger.getLogger(Thread2.class.getName()).log(Level.SEVERE, null, ex);
         }
